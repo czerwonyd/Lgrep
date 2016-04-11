61a74094a079aa7189365ec02ce7d20d458f21a8
Tomasz Strojny <tomasz@init.biz>
2016-04-11 18:20:22 +0200

    Good to go

--- lgrep.sh
+++ lgrep.sh
@@ -1,5 +1,50 @@
 #!/usr/bin/bash
 
+#TODO: function to check config file (are files present and writable)
+
+if [ "$#" == "0" ]; then
+    echo "Let's lgrep all of you."                                      #TODO: core of lgrep - lgrep them all
+    exit
+else
+    while getopts ":if:e:d:h" optname
+    do
+        case "$optname" in
+            "h")
+                echo " You can start lgrep using such parameters:"
+                echo "   <no parameters> - will lgrep all enabled files"
+                echo "   -h              - show this help"
+                echo "   -i              - initialize config files for files to lgrep"
+                echo "   -e <file path>  - enable config in filepath"
+                echo "   -i <file path>  - disable config in filepath"
+            ;;
+            "i")
+                echo "Initialize lgrep"                                 #TODO: function: copy directory tree from TO_FILTER_DIR to AVAILABLE_CONF_DIR and so on
+            ;;
+            "f")
+                echo "Option $optname with relative filepath: $OPTARG"
+            ;;
+            "e")
+                echo "Lgrep config $OPTARG enabled."                    #TODO: copy file from AVAILABLE_CONF_DIR to CONFIG_DIR and mkdirs if none present
+            ;;
+            "d")
+                echo "Lgrep config $OPTARG disabled."                   #TODO: remove config file from CONFIG_DIR and rmdir if last one
+            ;;
+            "?")
+                echo "Usage:"
+                echo "$0 [-i | -f <filepath> | -e <filepath> | -d <filepath>]"
+            ;;
+            ":")
+                echo "No argument value for option $OPTARG"
+            ;;
+            *)
+                # Should not occur
+                echo "Unknown error while processing options"
+            ;;
+        esac
+        #echo "OPTIND is now $OPTIND" #debug
+    done
+fi
+
 source lgrep_config.sh
 
 # function p_show() {
@@ -20,5 +65,8 @@ cd $LOG_DIR || {
    exit $E_XCD;
  }
 
+# (cd /var/log && find .) > /tmp/drzewo_varloga
+# (cd $CONFIG_DIR && find .) > /tmp/drzewo_configdira
+# for filename in drzewo_configdira { grep /tmp/drzewo_varloga } # tu będą te same
 
 exit 0
--- lgrep_config.sh
+++ lgrep_config.sh
@@ -1,4 +1,14 @@
-# path to directory where logs are located
-LOG_DIR=/var/log
+# path to directory where files to filter are located
+TO_FILTER_DIR=/var/log
 
-CONFIG_DIR=lgrep-enabled
+#path to directory where enabled configs are located
+CONF_DIR=lgrep-enabled
+
+#path to directory where available configs are located and config will be initiated
+AVAILABLE_CONF_DIR=lgrep-available
+
+#path to directory where filtered files will be stored.
+FILTERED_DIR=/root/lgrepped
+
+#path to directory where temporary files will be stored.
+TMP_DIR=/tmp
